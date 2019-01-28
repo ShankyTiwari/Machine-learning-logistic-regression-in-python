99b00a7e42d16d5f42d956c690aa4fdb126d102a
ShankyTiwari <shanshank69@gmail.com>
2019-01-28 12:40:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Machine learning logistic regression in python
+
+Creating a Model to predict if a user is going to buy the product or not based on a set of data, The logistic regression model created using pandas python library and scikit-learn library.
+
+
+# Code Explanation
+[http://www.codershood.info/2019/01/01/machine-learning-logistic-regression-in-python-with-an-example/](http://www.codershood.info/2019/01/01/machine-learning-logistic-regression-in-python-with-an-example/)
---
+++ logistic_regression_super_mall.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 29 23:10:15 2018
+Machine learning logistic regression in python with an example
+@author: SHASHANK
+"""
+
+# Importing the libraries
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+class Model:
+    X = None
+    Y = None
+    standardScaler = None
+   
+    # Importing the dataset
+    def importData(self):
+        dataset = pd.read_csv('Social_Network_Ads.csv')
+        self.X = dataset.iloc[:, [2,3]].values
+        self.Y = dataset.iloc[:, 4].values
+
+    # Applying feature scaling on the train data
+    def doFatureScaling(self):
+        self.standardScaler = StandardScaler()
+        self.X = self.standardScaler.fit_transform(self.X)
+
+    def isBuying(self):
+        self.importData()
+        self.doFatureScaling()
+        
+        # Fitting the Simple Linear Regression to the Training set
+        classifier = LogisticRegression(random_state = 0)
+        classifier.fit(self.X, self.Y)    
+        
+        userAge = float(raw_input("Enter the user's age? "))
+        userSalary = float(raw_input("What is the salary of user? "))
+        
+        # Applying feature scaling on the test data
+        testData = self.standardScaler.transform([[userAge, userSalary]])    
+        prediction = classifier.predict(testData)
+        
+        print 'This user is most likely to buy the product' if  prediction[0] == 1 else 'This user is not gonna buy the your product.'
+
+model = Model()
+model.isBuying()
